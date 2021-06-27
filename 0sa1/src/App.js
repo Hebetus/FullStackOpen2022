0a517501a3b7995704a3a571e893b77d8d01da15
Hebetus <emilhellberg20@gmail.com>
2021-06-27 15:54:08 +0300

    osa1 a ja b osa tähän väliin

---
+++ 0sa1/src/App.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+function App(props){
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+}
+
+  return (
+    <div>
+      <Course name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+function Course(props){
+	return <h1>{props.name}</h1>;
+}
+
+function Content(props){
+	return (
+    <>
+    <Part name={props.parts[0].name} amount={props.parts[0].exercises} />
+    <Part name={props.parts[1].name} amount={props.parts[1].exercises} />
+    <Part name={props.parts[2].name} amount={props.parts[2].exercises} />
+    </>
+	)
+}
+
+function Part(props){
+	return <p>{props.name} {props.amount}</p>;
+}
+
+function Total(props){
+	return <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>;
+}
+
+export default App
